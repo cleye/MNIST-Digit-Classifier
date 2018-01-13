d0c87dd1f2051c87701520911f04132044c8fed2
cleye <cleyejensen@gmail.com>
2018-01-13 08:36:45 +0800

    replaced trainingset class with data arrays, added status function

--- main.py
+++ main.py
@@ -9,58 +9,10 @@ import random
 from math import e, log
 
 
-class TrainingSet:
-	''' Training set from MNIST handwritten digit images
-
-	Attributes:
-		index: An integer index of the training set within the image file
-		y: A numpy column array of the desired neural net output
-		x: A numpy array of pixels in the training set image
-		image_2d: A numpy matrix of pixels in the training set image
-
-	'''
-
-	def __init__(self, index_):
-		pass
-		''' Inits TrainingSet by reading training set data '''
-		'''
-		self.LABELS = "/Users/cleye.jensen/Downloads/labels"
-		self.IMAGES = "/Users/cleye.jensen/Downloads/images"
-
-		self.index = index_
-
-		with open(self.LABELS, "rb") as labels:
-			# Position cursor at training set
-			labels.seek(self.index)
-			# Read 1 byte and convert to int
-			self.label = int.from_bytes(labels.read(1), byteorder="big")
-
-		# Set expected output from neural net
-		self.y = np.zeros(10)
-		self.y[self.label] = 1
-
-		# improve this
-		with open(self.IMAGES, "rb") as images:
-			# Position cursor at training set
-			images.seek(self.index * 784)
-			# 1D array of pixels
-			image = []
-
-			# Loop through rows and columns of image pixels
-			for i in range(28):
-				for i in range(28):
-					# Read 1 byte and convert to int
-					pixel = 1#int.from_bytes(images.read(1), byteorder="big")
-					image.append(pixel)
-
-		self.x = np.array(image)/255.
-
-	def display():'''
-		''' Displays the training set image '''
-		#plt.matshow(img)
-		#plt.show()
-
-
+LABELS_FILE = "/Users/cleye.jensen/Downloads/labels"
+IMAGES_FILE = "/Users/cleye.jensen/Downloads/images"
+TEST_IMAGES_FILE = "/Users/cleye.jensen/Downloads/test_images"
+TEST_LABELS_FILE = "/Users/cleye.jensen/Downloads/test_labels"
 
 
 # Number of layers
@@ -71,7 +23,7 @@ s = (None, 784, 25, 10)
 K = 10
 
 # Training sets
-m = 1000
+m = 5000
 # Features
 n = 784
 # Regularisation parameter
@@ -84,7 +36,19 @@ iteration = 0
 
 # Theta values initialised as random in bound [-10, 10]
 T1 = np.random.random((s[2], s[1]+1)) * 0.2 - 0.1
-T2 = np.random.random((s[3], s[2]+1)) * 0.2 - 0.1
+T2 = np.random.random((s[3], s[2]+1)) * 2 - 1
+
+
+# Set X and Y values
+Y = np.zeros((m,K))
+# Temporary counter
+i = 0
+for y in np.fromfile(LABELS_FILE, dtype=np.uint8, count=m):
+	Y[i][y] = 1
+	i+=1
+del i
+X = np.reshape(np.fromfile(IMAGES_FILE, dtype=np.uint8, count=m*784), (m, 28, 28))/255.
+
 
 
 def g(z):
@@ -94,15 +58,15 @@ def g(z):
 
 def h(t):
 	''' Hypothesis function, returns output layer of neural network '''
-	return activiation_units(t)[3]
+	return activation_units(t)[3]
 
 
-def activiation_units(t):
+def activation_units(t):
 	''' Return array of each layer's activation units for training set '''
 
 	# LAYER 1 (input layer)						# Example output layer representation
 	# Add bias unit to training set input			# 1  [ 0.2134 ] 
-	x = np.insert(t.x, 0, 1)						# 2  [ 0.6345 ] 
+	x = np.insert(X[t], 0, 1)						# 2  [ 0.6345 ] 
 													# 3  [ 0.2224 ] 
 	# LAYER 2 (hidden layer)						# 4  [ 0.2064 ] 
 	# Sum up parameters with dot product			# 5  [ 0.9554 ] 
@@ -127,7 +91,7 @@ def errors(t,a):
 	''' Returns array of layer 2 and 3 errors for training set, t '''
 
 	# Layer 3 errors
-	e3 = a[3] - t.y
+	e3 = a[3] - Y[t]
 
 	# Layer 2 errors
 	e2 = np.dot(T2.T, e3) * (a[2] * (1 - a[2]))
@@ -144,16 +108,15 @@ def J():
 
 	# Sum for hypothesis cost
 	h_sum = 0
-	for i in range(m):
-
-		t = TrainingSet(i)
+	# Loop through training sets
+	for t in range(m):
 		# Hypothesis for training set
 		h_i = h(t)
 
 		for k in range(K):
-			if t.y[k] == 1:
+			if Y[t][k] == 1:
 				h_sum += -log(h_i[k])
-			if t.y[k] == 0:
+			if Y[t][k] == 0:
 				h_sum += -log(1 - h_i[k])
 
 	# Sum for parameter cost
@@ -175,17 +138,17 @@ def J():
 	return (h_sum/m) + l*p_sum/(2*m) 
 
 
-def gradient_check(D):
+def gradient_check(D, amount=5):
 	''' Print approximated values for parameter derivatives of T2
 		and compare with calculated derivatives '''
 
 	# Amount of gradients to check
-	gradients_amount = 5
+	gradients_amount = amount
 
 	for i in range(gradients_amount):
 
 		# Small change in theta
-		q = 0.0001
+		q = 0.00001
 
 		# Random indices
 		x = random.randint(0, s[2]-1)
@@ -214,24 +177,23 @@ def iterate(verbose=False):
 		Performs backpropagation to calculate parameter derivatives for one iteration
 		Returns array of parameter derivatives for the iteration '''
 
-	global T1,T2, iteration
+	global T1, T2, iteration
 
 	# Accumulator for parameter derivatives
 	d1 = np.zeros_like(T1)
 	d2 = np.zeros_like(T2)
 
 	# Loop through training sets
-	for i in range(m):
+	for t in range(m):
 
-		t = TrainingSet(i)
-
-		a = activiation_units(t)
+		a = activation_units(t)
 
 		E = errors(t,a)
 
 		# atleast_2d enables arrays to tranpose and perform dot product
 		# E2 removes bias unit with slice
 		E2 = np.atleast_2d(E[2][1:]).T
+		# E3 is the error of last layer, no bias unit
 		E3 = np.atleast_2d(E[3]).T
 
 		a1 = np.atleast_2d(a[1])
@@ -266,28 +228,39 @@ def iterate(verbose=False):
 	return D2
 
 
-def status():
-	#print "-"*16
-	print "ITERATION #{} : {}".format(iteration,J())
-	#print "Cost: \t", J()
-	#print ""
-	#print "-"*16
+def status(percentage_correct=False, fancy=False):
+	''' Prints status of neural net such as cost and images correct 
+	Parameters:
+		percentage_correct: 
+		fancy: Whether to use fancy formatting for status
+	'''
+	if fancy:
+		print "-"*16
+		print "ITERATION #{}".format(iteration)
+		print "Cost: \t", J()
+		print "-"*16
+
+	if not fancy:
+		print "ITERATION #{} : {}".format(iteration,J())
+
+	if percentage_correct:
+		# Test random hypotheses
+		for i in range(10):
+			print "Hypothesis: ", np.argmax(h(i)), "\tActual:", np.argmax(Y[i])
+
+
 
 np.set_printoptions(threshold=1000000,suppress=True)
 
 def run():
-	iterate()
-	status()
-	D = iterate()
-	gradient_check(D)
+	for i in range(6):
+		for i in range(49):
+			iterate()
+			status()
+		iterate()
+		status(percentage_correct=True, fancy=True)
 
-
-labels = np.fromfile("/Users/cleye.jensen/Downloads/labels", dtype=np.uint8)
-images = np.reshape(np.fromfile("/Users/cleye.jensen/Downloads/images", dtype=np.uint8, count=m*784), (m, 28, 28))
-
-
-
-
+run()
 
 
 '''
@@ -295,7 +268,10 @@ img = tset(0)[2]
 plt.matshow(img)
 plt.show()
 
-
+Problems:
+	overflow error in g(z)
+	second layer units are 1s or 0s
+	J() potential overflow error
 
 '''
 
