abc92b980c27e8579f66de8dc93341bf01a30dc3
cleye <cleyejensen@gmail.com>
2018-01-14 14:08:14 +0800

    add OOP interface for the neural network

--- main.py
+++ main.py
@@ -15,31 +15,7 @@ TEST_IMAGES_FILE = "/Users/cleye.jensen/Downloads/test_images"
 TEST_LABELS_FILE = "/Users/cleye.jensen/Downloads/test_labels"
 
 
-# Number of layers
-L = 3
-# Units in each layer
-s = (None, 784, 25, 10)
-# Number of output layers (None is added to make indices more logical)
-K = 10
-
-# Number of training sets
-m = 5000
-m_test = 1000
-# Features
-n = 784
-# Regularisation parameter
-l = 1
-# Learning rate
-A = 0.05
-# Number of iterations
-iteration = 0
-
-
-# Theta values initialised as random in bound [-10, 10]
-T1 = np.random.random((s[2], s[1]+1)) * 0.2 - 0.1
-T2 = np.random.random((s[3], s[2]+1)) * 2 - 1
-
-
+g = lambda z: (1 / (1 + e**-z)) 
 
 def get_data(images_file, labels_file, training_sets):
 	''' Get data from MNIST handwritten digit images and labels 
@@ -48,7 +24,7 @@ def get_data(images_file, labels_file, training_sets):
 	'''
 
 	# Initialise y values as all zeros
-	y = np.zeros((training_sets,K))
+	y = np.zeros((training_sets, 10))
 
 	with open(labels_file, "rb") as labels:
 		# Skip the first data bytes
@@ -68,226 +44,256 @@ def get_data(images_file, labels_file, training_sets):
 	
 	return x,y
 
-X, Y = get_data(IMAGES_FILE, LABELS_FILE, m)
-X_test, Y_test = get_data(TEST_IMAGES_FILE, TEST_LABELS_FILE, m_test)
 
-def g(z):
-	''' Sigmoid function '''
-	return (1  / (1 + e**-z)) 
+class NeuralNetwork:
 
+	def __init__(self):
 
-def h(t):
-	''' Hypothesis function, returns output layer of neural network '''
-	return activation_units(t)[3]
-
-
-def activation_units(t):
-	''' Return array of each layer's activation units for training set '''
-
-	# LAYER 1 (input layer)		
-	# Add bias unit to training set input
-	if t >= 0:
-		x = np.insert(X[t], 0, 1)
-	else:
-		# Negative t indicates test training set
-		x = np.insert(X_test[~t], 0, 1)
-										
-	# LAYER 2 (hidden layer)			
-	# Sum up parameters with dot product
-	z = np.dot(T1, x)					
-	# Activation units for layer 2		
-	a2 = g(z)							
-	# Add bias units 					
-	a2 = np.insert(a2, 0, 1)			
-
-	# LAYER 3 (output layer)
-	# Sum up parameters with dot product
-	z = np.dot(T2, a2)
-	# Activation units for layer 3 (output units)	
-	a3 = g(z)
-
-	# Return all activation units
-	# None is added to make indices more logical
-	return (None, x, a2, a3)
+		# Number of layers
+		self.L = 3
+		# Units in each layer (None is added to make indices more logical)
+		self.s = (None, 784, 25, 10)
+		# Number of output layers 
+		self.K = 10
 
 
-def errors(t,a):
-	''' Returns array of layer 2 and 3 errors for training set, t '''
-
-	# Layer 3 errors
-	e3 = a[3] - Y[t]
-
-	# Layer 2 errors
-	e2 = np.dot(T2.T, e3) * (a[2] * (1 - a[2]))
-
-	# None is added to make indices more logical
-	return (None, None, e2, e3)
 
 
-def J():
-	''' Regularised cost function has two components:
-	1. the hypothesis cost
-	2. the parameter cost (for regularisation) '''
 
 
-	# Sum for hypothesis cost
-	h_sum = 0
-	# Loop through training sets
-	for t in range(m):
-		# Hypothesis for training set
-		h_i = h(t)
+	def train(self, examples=5000, learning_rate=0.01, regularisation_param=0.5, until_cost=1.23, until_difference=0.001, until_iteration=1000, status="normal"):
 
-		for k in range(K):
-			if Y[t][k] == 1:
-				h_sum += -log(h_i[k])
-			if Y[t][k] == 0:
-				h_sum += -log(1 - h_i[k])
+		# Number of training sets
+		self.m = examples
+		# Features
+		self.n = 784
+		# Regularisation parameter
+		self.l = regularisation_param
+		# Learning rate
+		self.A = learning_rate
+		# Number of iterations
+		self.iteration = 0
 
-	# Sum for parameter cost
-	p_sum = 0
+		# Theta values initialised as random
+		self.T1 = np.random.random((self.s[2], self.s[1]+1)) * 0.2 - 0.1
+		self.T2 = np.random.random((self.s[3], self.s[2]+1)) * 2 - 1
 
-	# Add regularisation cost of theta1 parameters
-	for i in T1:
-		# Slice to exclude bias unit
-		for t in i[:-1]:
-			p_sum += t**2
+		# Images and labels from MNIST dataset put into X and Y variables
+		self.X, self.Y = get_data(IMAGES_FILE, LABELS_FILE, self.m)
+
+
+	def test(self, examples=1000):
+
+		self.m_test = examples
+
+		self.X_test, self.Y_test = get_data(TEST_IMAGES_FILE, TEST_LABELS_FILE, self.m_test)
+
+
+
+
+	def h(self, t):
+		''' Hypothesis function, returns output layer of neural network '''
+		return self.activation_units(t)[3]
+
+
+	def activation_units(t):
+		''' Return array of each layer's activation units for training set '''
+
+		# LAYER 1 (input layer)		
+		# Add bias unit to training set input
+		if t >= 0:
+			x = np.insert(self.X[t], 0, 1)
+		else:
+			# Negative t indicates test training set
+			x = np.insert(self.X_test[~t], 0, 1)
+											
+		# LAYER 2 (hidden layer)			
+		# Sum up parameters with dot product
+		z = np.dot(self.T1, x)					
+		# Activation units for layer 2		
+		a2 = self.g(z)							
+		# Add bias units 					
+		a2 = np.insert(a2, 0, 1)			
+
+		# LAYER 3 (output layer)
+		# Sum up parameters with dot product
+		z = np.dot(self.T2, a2)
+		# Activation units for layer 3 (output units)	
+		a3 = self.g(z)
+
+		# Return all activation units
+		# None is added to make indices more logical
+		return (None, x, a2, a3)
+
+
+	def errors(t,a):
+		''' Returns array of layer 2 and 3 errors for training set, t '''
+
+		# Layer 3 errors
+		e3 = a[3] - self.Y[t]
+
+		# Layer 2 errors
+		e2 = np.dot(self.T2.T, e3) * (a[2] * (1 - a[2]))
+
+		# None is added to make indices more logical
+		return (None, None, e2, e3)
+
+
+	def J():
+		''' Regularised cost function has two components:
+		1. the hypothesis cost
+		2. the parameter cost (for regularisation) '''
+
+
+		# Sum for hypothesis cost
+		h_sum = 0
+		# Loop through training sets
+		for t in range(self.m):
+			# Hypothesis for training set
+			h_i = self.h(t)
+
+			for k in range(self.K):
+				if self.Y[t][k] == 1:
+					h_sum += -log(h_i[k])
+				if self.Y[t][k] == 0:
+					h_sum += -log(1 - h_i[k])
+
+		# Sum for parameter cost
+		p_sum = 0
+
+		# Add regularisation cost of theta1 parameters
+		for i in self.T1:
+			# Slice to exclude bias unit
+			for t in i[:-1]:
+				p_sum += t**2
+				
+
+		# Add regularisation cost of theta2 parameters 
+		for i in self.T2:
+			# Slice to exclude bias unit
+			for t in i[:-1]:
+				p_sum += t**2
+
+		return (h_sum/self.m) + l*p_sum/(2*self.m) 
+
+
+	def gradient_check(D, amount=5):
+		''' Print approximated values for parameter derivatives of T2
+			and compare with calculated derivatives '''
+
+		# Amount of gradients to check
+		gradients_amount = amount
+
+		for i in range(gradients_amount):
+
+			# Small change in theta
+			q = 0.00001
+
+			# Random indices
+			x = random.randint(0, s[2]-1)
+			y = random.randint(0, s[3]-1)
+
+			# Underestimate of cost function
+			self.T2[y][x] -= q
+			J_under = self.J()
+
+			# Overestimate of cost function
+			self.T2[y][x] += 2*q
+			J_over = self.J()
+
+			# Back to original value
+			self.T2[y][x] -= q
+
+			# Approximate gradient
+			approx_grad = (J_over - J_under) / (2*q)
+
+			print "Approximate gradient =\t", approx_grad
+			print "Calculated gradient =\t", D[y][x], "\n"
+
+
+	def iterate(verbose=False):
+		''' Calculates activation units, errors for nodes,
+			Performs backpropagation to calculate parameter derivatives for one iteration
+			Returns array of parameter derivatives for the iteration '''
+
+		# Accumulator for parameter derivatives
+		d1 = np.zeros_like(self.T1)
+		d2 = np.zeros_like(self.T2)
+
+		# Loop through training sets
+		for t in range(self.m):
+
+			a = activation_units(t)
+
+			E = errors(t,a)
+
+			# atleast_2d enables arrays to tranpose and perform dot product
+			# E2 removes bias unit with slice
+			E2 = np.atleast_2d(E[2][1:]).T
+			# E3 is the error of last layer, no bias unit
+			E3 = np.atleast_2d(E[3]).T
+
+			a1 = np.atleast_2d(a[1])
+			a2 = np.atleast_2d(a[2])
 			
-
-	# Add regularisation cost of theta2 parameters 
-	for i in T2:
-		# Slice to exclude bias unit
-		for t in i[:-1]:
-			p_sum += t**2
-
-	return (h_sum/m) + l*p_sum/(2*m) 
-
-
-def gradient_check(D, amount=5):
-	''' Print approximated values for parameter derivatives of T2
-		and compare with calculated derivatives '''
-
-	# Amount of gradients to check
-	gradients_amount = amount
-
-	for i in range(gradients_amount):
-
-		# Small change in theta
-		q = 0.00001
-
-		# Random indices
-		x = random.randint(0, s[2]-1)
-		y = random.randint(0, s[3]-1)
-
-		# Underestimate of cost function
-		T2[y][x] -= q
-		J_under = J()
-
-		# Overestimate of cost function
-		T2[y][x] += 2*q
-		J_over = J()
-
-		# Back to original value
-		T2[y][x] -= q
-
-		# Approximate gradient
-		approx_grad = (J_over - J_under) / (2*q)
-
-		print "Approximate gradient =\t", approx_grad
-		print "Calculated gradient =\t", D[y][x], "\n"
-
-
-def iterate(verbose=False):
-	''' Calculates activation units, errors for nodes,
-		Performs backpropagation to calculate parameter derivatives for one iteration
-		Returns array of parameter derivatives for the iteration '''
-
-	global T1, T2, iteration
-
-	# Accumulator for parameter derivatives
-	d1 = np.zeros_like(T1)
-	d2 = np.zeros_like(T2)
-
-	# Loop through training sets
-	for t in range(m):
-
-		a = activation_units(t)
-
-		E = errors(t,a)
-
-		# atleast_2d enables arrays to tranpose and perform dot product
-		# E2 removes bias unit with slice
-		E2 = np.atleast_2d(E[2][1:]).T
-		# E3 is the error of last layer, no bias unit
-		E3 = np.atleast_2d(E[3]).T
-
-		a1 = np.atleast_2d(a[1])
-		a2 = np.atleast_2d(a[2])
-		
-		# Parameter derivatives accumulation for layers 2 and 3
-		d1 += np.dot(E2, a1)
-		d2 += np.dot(E3, a2)
+			# Parameter derivatives accumulation for layers 2 and 3
+			d1 += np.dot(E2, a1)
+			d2 += np.dot(E3, a2)
 
 
 
-	# Uses matrix operations to exclude bias unit from being regularised
-	# Creates a matrix of ones corresponding to parameters
-	bias_unit_discriminator = np.ones_like(T1)
-	# Set parameters involving bias unit = 0
-	bias_unit_discriminator.T[-1] = np.zeros(s[2])
-	# Average derivatives and regularise
-	D1 = d1/m + l*T1*bias_unit_discriminator/(2*m)
+		# Uses matrix operations to exclude bias unit from being regularised
+		# Creates a matrix of ones corresponding to parameters
+		bias_unit_discriminator = np.ones_like(self.T1)
+		# Set parameters involving bias unit = 0
+		bias_unit_discriminator.T[-1] = np.zeros(s[2])
+		# Average derivatives and regularise
+		D1 = d1/self.m + self.l*self.T1*bias_unit_discriminator/(2*self.m)
 
-	# Ditto
-	bias_unit_discriminator = np.ones_like(T2)
-	bias_unit_discriminator.T[-1] = np.zeros(s[3])
-	D2 = d2/m + l*T2*bias_unit_discriminator/(2*m)
-
-
-	# Update parameters
-	T1 -= A * D1
-	T2 -= A * D2
-
-	iteration += 1
-
-	return D2
+		# Ditto
+		bias_unit_discriminator = np.ones_like(self.T2)
+		bias_unit_discriminator.T[-1] = np.zeros(s[3])
+		D2 = d2/self.m + self.l*self.T2*bias_unit_discriminator/(2*self.m)
 
 
-def status(percentage_correct=False, fancy=False):
-	''' Prints status of neural net such as cost and images correct 
-	Parameters:
-		percentage_correct: 
-		fancy: Whether to use fancy formatting for status
-	'''
-	if fancy:
-		print "-"*16
-		print "ITERATION #{}".format(iteration)
-		print "Cost: \t", J()
-		print "-"*16
+		# Update parameters
+		self.T1 -= self.A * D1
+		self.T2 -= self.A * D2
 
-	if not fancy:
-		print "ITERATION #{} : {}".format(iteration,J())
+		self.iteration += 1
 
-	if percentage_correct:
-		correct = 0
-		# Test if hypotheses is same as actual value
-		for i in range(m_test):
-			if np.argmax(h(~i)) == np.argmax(Y_test[i]):
-				correct += 1
-
-		print correct, "/100 ({}%) correct".format(100*correct/m_test)
+		return D2
 
 
-def display_guess(t):
-	''' Display the digit and neural network tries to guess it '''
+	def status(self, fancy=False):
+		''' Prints status of neural net such as cost and images correct 
+		Parameters:
+			percentage_correct: 
+			fancy: Whether to use fancy formatting for status
+		'''
 
-	# Creates 2d array from pixels 
-	x2d = np.reshape(X_test[t], (28,28))
+		if fancy:
+			print "-"*16
+			print "ITERATION #{}".format(self.iteration)
+			print "Cost: \t", J()
+			print "-"*16
 
-	plt.matshow(x2d)
-	plt.suptitle(np.argmax(h(~t)))
-	plt.show()
+		if not fancy:
+			print "ITERATION #{} : {}".format(self.iteration,self.J())
 
 
+	'''def display_guess(t):
+		 Display the digit and neural network tries to guess it 
+
+		# Creates 2d array from pixels 
+		x2d = np.reshape(X_test[t], (28,28))
+
+		plt.matshow(x2d)
+		plt.suptitle(np.argmax(h(~t)))
+		plt.show()'''
+
+N = NeuralNetwork()
+N.train()
 
 
 '''
